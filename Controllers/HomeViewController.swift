b8e56b8610520ecd4fbbff9579dd0c7fc6d3244a
Vladlens Kukjans <kukjanvladlen@gmail.com>
2023-03-13 21:28:28 +0000

    add header view

--- Controllers/HomeViewController.swift
+++ Controllers/HomeViewController.swift
@@ -8,13 +8,94 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let homeFeedTable: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+       
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+        
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       
         view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+        
+      
+        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
+        homeFeedTable.tableHeaderView = headerView
+        
+        configureNavBar()
     }
     
+    private func configureNavBar() {
+       
+      
+        var image = UIImage(named: "nl")?.withRenderingMode(.alwaysOriginal)
+        image = image?.withRenderingMode(.alwaysOriginal)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image?.resizeTo(size: CGSize(width: 20, height: 30)), style: .done, target: self, action: nil)
+     
+    
+    }
+     
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        homeFeedTable.frame = view.bounds
+    }
+   
+    
 
-
+}
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+   
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+           
+        }
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        40
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
+    }
+    
+}
+// :MARK - Extension to resize UIImage ,to fit UIBarButtonItem
+extension UIImage {
+    
+    func resizeTo(size: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        let image = renderer.image { _ in
+            self.draw(in: CGRect.init(origin: CGPoint.zero, size: size))
+        }
+        
+        return image.withRenderingMode(self.renderingMode)
+    }
 }
---
+++ Views/HeroHeaderUIView.swift
@@ -0,0 +1,90 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by Vladlens Kukjans on 13/03/2023.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    
+    let downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        //image.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private func addGradient() {
+    let gradientLayer = CAGradientLayer()
+        gradientLayer.colors =  [
+            
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        
+        layer.addSublayer(gradientLayer)
+        gradientLayer.frame = bounds
+    }
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+        addSubview(playButton)
+        addSubview(downloadButton)
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+        
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 65),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            playButton.widthAnchor.constraint(equalToConstant: 120),
+                        
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -65),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 120),
+        ])
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
