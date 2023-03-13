771d5ff83377954290110dbef94dc0f1007e899d
Vladlens Kukjans <kukjanvladlen@gmail.com>
2023-03-13 16:24:24 +0000

    Add Tab Bar Controllers

---
+++ Controllers/UpcomingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Vladlens Kukjans on 13/03/2023.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+  
+
+}
